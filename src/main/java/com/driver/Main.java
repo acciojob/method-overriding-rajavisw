36dfe8f358649a74ca9bab20c51df5b4376ccf33
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-01 17:47:55 +0530

    Assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     B b=new B();
     String ans=b.meth();
+    String ans1=b.meth();
     public class A{
         public String meth(){
             return "Invoking method from class A";
